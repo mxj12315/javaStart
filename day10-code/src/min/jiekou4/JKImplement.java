0686275706c451d018ea9d3b52f0aca27d8a05ab
min <mxj12315@qq.com>
2021-05-08 07:00:20 +0800

    更新

---
+++ day10-code/src/min/jiekou4/JKImplement.java
@@ -0,0 +1,10 @@
+package min.jiekou4;
+
+public class JKImplement implements JKTest{
+    public static void main(String[] args) {
+        JKTest jkTest =new JKImplement();
+        jkTest.run();
+        jkTest.eat();
+    }
+
+}
--- day10-code/src/min/jiekou4/JKTest.java
+++ day10-code/src/min/jiekou4/JKTest.java
@@ -21,18 +21,24 @@ public interface JKTest {
     // 改造后
     default void run() {
         int a = 10;
+        System.out.println(a);
         go();
     }
 
 
     default void eat() {
         String name = "马化腾";
+        System.out.println(name);
         go();
     }
 
     private void go(){
         // 公共代码
-        System.out.println(123);
+        System.out.println("我是private修饰的公共代码");
+    }
+
+     static void main(String[] args) {
+
     }
 
 }
